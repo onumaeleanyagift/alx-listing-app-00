ab66678fab8e89aac58d063a7dd36df978df1173
Eleanya Onuma <onumaeleanyagift@gmail.com>
2025-08-02 12:47:15 +0100

    feat(components): create Pill component to render filter props as clickable pills

---
+++ components/Pill.tsx
@@ -0,0 +1,19 @@
+// components/Pill.tsx
+import React from "react";
+import { PillProps } from "../interfaces";
+
+
+const Pill: React.FC<PillProps> = ({ label, onClick, isActive }) => {
+  return (
+    <button
+      onClick={onClick}
+      className={`px-4 py-2 rounded-full border text-sm font-medium transition ${
+        isActive ? "bg-black text-white" : "bg-white text-black border-gray-300"
+      } hover:bg-black hover:text-white`}
+    >
+      {label}
+    </button>
+  );
+};
+
+export default Pill;
